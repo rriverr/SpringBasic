2db4124fbe08fd150f4ac1e8c407adf16696c86f
yuna <playamongthestars000@gmail.com>
2023-09-05 10:59:12 +0900

    학사관리 시스템 응용 - MVC패턴 적용하기

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 - **ch~ : 수업**
   - ch02 ~ : 계산기
   - ch03 ~ : 의존성 주입, spring context
-  - ch04 ~ : Ioc. 학사관리 시스템 만들기
+  - ch04 ~ : Ioc, 학사관리 시스템 만들기, spring-context scope
 
     
 - **test : 실습**
   - test001 : 계산기 실습
-  - test002 : spring context 실습
+  - test002 : spring-context 실습
+  - test003 : 학사관리 시스템 응용 - 수정 날짜 남기기
---
+++ test003/src/main/java/test003/exercise/controller/MemberController.java
@@ -0,0 +1,55 @@
+package test003.exercise.controller;
+
+import test003.exercise.view.MemberViewer;
+import test003.exercise.dao.MemberDO;
+import test003.exercise.dao.MemberDAO;
+
+public class MemberController {
+	private MemberDAO memberDao;
+	private MemberViewer memberViewer;
+	
+	public MemberController(MemberDAO memberDao){
+		this.memberDao = memberDao;
+	}
+	
+	public void insert(MemberDO member) {
+		if(verify(member.getId())) {
+			memberDao.insert(member);
+		}else {
+			memberViewer.showMessage("This member has already been registered\n");
+		}
+	}
+	
+	public void delete(String memberId) {
+		if(!verify(memberId)) {
+			memberDao.delete(memberId);
+		}else {
+			memberViewer.showMessage("Delete failed\n");
+		}
+	}
+	
+	public void showSelectedMember(String memberId){
+		if(!verify(memberId)) {
+			memberViewer.showMember(memberDao.select(memberId));
+		}else {
+			memberViewer.showMessage("There is no student who you are looking for\n");
+		}
+	}
+	
+	public void modifySelectedMember(String memberId) {
+		if(!verify(memberId)) {
+			memberDao.update(memberDao.select(memberId));
+		}else {
+			memberViewer.showMessage("There is no student who you are trying to modify\n");
+		}
+	}
+	
+	public void showAllMembers() {
+		memberViewer.showAllMembers(memberDao.getMembers());
+	}
+	
+	public boolean verify(String id) {
+		MemberDO member = memberDao.select(id);
+		return member == null ? true : false;
+	}
+}
